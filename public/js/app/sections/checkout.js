a96317bedf2e7d95d2db14f7101a6e5f5589bb12
Bastian Englert <11964168+1Englert1@users.noreply.github.com>
2018-11-25 20:31:42 +0100

    button change color and spacing to icon

--- public/js/app/sections/checkout.js
+++ public/js/app/sections/checkout.js
@@ -70,7 +70,7 @@ export default {
                     <div class="button-container">
                         <a v-if="isScreenshot" :href="screenshot" class="btn custom-action" role="button" aria-disabled="true" download><i class="fa fa-arrow-circle-down"></i> Download screenshot</a>
 
-                        <button v-if="sharingSupported" v-on:click="share" class="btn custom-action" role="button" aria-disabled="true"><i class="fa fa-shopping-cart"></i>Share</button>
+                        <button v-if="sharingSupported" v-on:click="share" class="btn custom-standard" role="button" aria-disabled="true"><i class="fa fa-shopping-cart"></i> Share</button>
                         <a v-if="shopEnabled" :href="image.shopURL" class="btn custom-action" role="button" aria-disabled="true"><i class="fa fa-shopping-cart"></i>Order print</a>
                     </div>
                 </div>
