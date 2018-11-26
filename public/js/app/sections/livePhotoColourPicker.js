2692ef5b78ef8d31f59243dc74174b98b2b89190
Bastian Englert <11964168+1Englert1@users.noreply.github.com>
2018-11-26 22:41:16 +0100

    change lable

--- public/js/app/sections/livePhotoColourPicker.js
+++ public/js/app/sections/livePhotoColourPicker.js
@@ -137,10 +137,9 @@ Take a photo
   <div class="ml-auto  bd-highlight">
   <div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle custom-switch" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <i class="fas fa-palette"></i>  By color
-    </button>
+      <i class="fas fa-camera"></i>  By photo</button>
     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-      <a class="dropdown-item" href="app.html#/photo-input"><i class="fas fa-camera"></i> By photo</a>
+      <a class="dropdown-item" href="app.html#/photo-input"><i class="fas fa-pallete"></i> By color</a>
     </div>
   </div>
 </div>
