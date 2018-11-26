087ca05918e03ab2d44e7894e5ebf84008a73d91
Bastian Englert <11964168+1Englert1@users.noreply.github.com>
2018-11-26 20:28:56 +0100

    delet border 0 at questions

--- public/js/app/sections/question.js
+++ public/js/app/sections/question.js
@@ -13,7 +13,7 @@ export default {
   template: `
    <div class="container-fluid">
     <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
-      <button class="navbar-toggler border-0 p-0" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+      <button class="navbar-toggler p-0" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
         aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
