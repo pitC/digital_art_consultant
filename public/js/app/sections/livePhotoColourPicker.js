8a6d71af661e2cc8cfd662f073a1a64f705ceb45
Bastian Englert <11964168+1Englert1@users.noreply.github.com>
2018-11-26 22:35:35 +0100

    color detection switch in dynamic live photo

--- public/js/app/sections/livePhotoColourPicker.js
+++ public/js/app/sections/livePhotoColourPicker.js
@@ -134,7 +134,16 @@ export default {
   <span class="navbar-text">
 Take a photo
   </span>
-
+  <div class="ml-auto  bd-highlight">
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle custom-switch" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-palette"></i>  By color
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="app.html#/photo-input"><i class="fas fa-camera"></i> By photo</a>
+    </div>
+  </div>
+</div>
 </nav>
     <p class="bg-light" v-if="debug">{{debugStr}} Dragging:{{draggingMode}}<span class='badge badge-primary' :style='style'>{{markedColour}}</span></p>
     <div>
